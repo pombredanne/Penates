142d75cb7b603e06037de0523479728233c7b149
mgallet <mgallet@spibol.19pouces.net>
2015-08-13 14:07:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+Penates
+=======
+
+Group of Ansible tasks for configuring a basic private network with the following services:
+
+    * PKI (openssl)
+    * LDAP (openldap)
+    * DHCP (isc-dhcp-server)
+    * DNS (powerdns)
+    * NTP (ntpd)
+    * Kerberos (MIT kerberos)
+    * webservices (PenatesServer)
+    
+    
+Newly installed machines should register themselves to these services:
+
+    * create a DHCP entry for each MAC to fix IP addresses
+    * create a DNS entry for the fqdn
+    * create a certificate
+    * crate a Kerberos principal for the HOST/ keytab
+    * create a netgroup LDAP entry for admin rights
+    
---
+++ filter_plugins/penates.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import netaddr
+
+__author__ = 'Matthieu Gallet'
+
+
+def hostname_to_dn(fqdn):
+    """Convert a fqdn to its LDAP form
+
+    >>> hostname_to_dn("test.example.com")
+    u'dc=test,dc=example,dc=com'
+
+    """
+    return ','.join(['dc=' + x for x in fqdn.split('.')])
+
+
+def hostname_to_dn_first(fqdn):
+    """Convert a fqdn to its LDAP form
+
+    >>> hostname_to_dn_first("test.example.com")
+    u'test'
+
+    """
+    return fqdn.split('.')[0]
+
+
+def str_to_hex(text):
+    """
+
+    >>> str_to_hex("toto")
+    u'{HEX}746f746f'
+    """
+    return '{HEX}' + ''.join(['%x' % ord(x) for x in text])
+
+
+def subnet_mask(subnet):
+    """
+    >>> subnet_mask('192.168.56.1/24')
+    '255.255.255.0'
+    """
+    network = netaddr.IPNetwork(subnet)
+    return str(network.netmask)
+
+
+def subnet_mask_len(subnet):
+    """
+    >>> subnet_mask_len('192.168.56.1/24')
+    '24'
+    """
+    network = netaddr.IPNetwork(subnet)
+    return str(network.prefixlen)
+
+
+def subnet_address(subnet):
+    """
+    >>> subnet_address('192.168.56.1/24')
+    '192.168.56.0'
+    """
+    network = netaddr.IPNetwork(subnet)
+    return str(network.network)
+
+
+def subnet_broadcast(subnet):
+    """
+    >>> subnet_broadcast('192.168.56.1/24')
+    '192.168.56.255'
+    """
+    network = netaddr.IPNetwork(subnet)
+    return str(network.broadcast)
+
+
+def subnet_start(subnet):
+    """
+    >>> subnet_start('192.168.56.1/24')
+    '192.168.56.32'
+    """
+    network = netaddr.IPNetwork(subnet)
+    size = 32 if network.version == 4 else 128
+    return str(network.network + 2 ** max(size - network.prefixlen - 3, 0))
+
+
+def subnet_end(subnet):
+    """
+    >>> subnet_end('192.168.56.1/24')
+    '192.168.56.254'
+    """
+    network = netaddr.IPNetwork(subnet)
+    size = 32 if network.version == 4 else 128
+    return str(network.network - 2 + 2 ** (size - network.prefixlen))
+
+
+class FilterModule(object):
+    # noinspection PyMethodMayBeStatic
+    def filters(self):
+        return {'hostname_to_dn': hostname_to_dn,
+                'hostname_to_dn_first': hostname_to_dn_first,
+                'str_to_hex': str_to_hex,
+                'subnet_mask_len': subnet_mask_len,
+                'subnet_mask': subnet_mask,
+                'subnet_address': subnet_address,
+                'subnet_broadcast': subnet_broadcast,
+                'subnet_start': subnet_start,
+                'subnet_end': subnet_end,
+                }
